913fb027888c28b465324c3a9c93d8338d58c5c6
Richard1022 <717388657@qq.com>
2018-05-23 15:42:55 +0800

    2018/5/23 push throttle

--- common.js
+++ common.js
@@ -16,3 +16,34 @@ export function type(obj) {
         classType[Object.prototype.toString.call(obj)] || 'object' :
         typeof obj;
 }
+
+export const toString = Object.prototype.toString
+type.call('richard')
+
+// 为了避免1.某些间隔被跳过2.间隔可能比预期的小 这个两个问题
+// 你可以使用链式setTimeout()调用
+setTimeout(function () {
+    TODO();
+
+    setTimeout(arguments.callee, interval);
+}, interval)
+
+// 函数节流
+function throttle(method, context) {
+    clearTimeout(method.tId);
+    method.tId = setTimeout(function () {
+        method.call(context);
+    }, 100)
+}
+// 测试用例
+function resizeDiv() {
+    let div = document.getElementById('div');
+    div.style.height = div.offsetWidth + "px";
+}
+// call没有指定运行上下文 默认为全局global
+window.onresize = function () {
+    throttle(resizeDiv);
+}
+
+
+
--- function.js
+++ function.js
@@ -19,6 +19,15 @@ function curry(fn, args) {
         }
     }
 }
+function curry(fn) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return function () {
+        let innerArgs = Array.prototype.slice.call(arguments);
+        let finalArgs = args.concat(innerArgs);
+        return fn.apply(null, finalArgs);
+    }
+}
+
 var fn = curry(function (a, b, c) {
     console.log([a, b, c]);
 });
