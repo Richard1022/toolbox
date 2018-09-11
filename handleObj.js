0a3701e78af3b7bcc55fa4f9d6e1213fbcf02e91
richard <717388657@qq.com>
2018-09-11 16:50:48 +0800

    <修复es6>

--- handleObj.js
+++ handleObj.js
@@ -3,20 +3,20 @@
  * @param {object} obj copy的对象
  * @return {object} 深拷贝返回对象
  */
-export function deepClone(obj) {
-    if (obj === null) return null
+export function deepCloneJson(obj) {
+    if (obj === null) return null;
     if (typeof obj !== 'object') return obj;
     if (obj.constructor === Date) return new Date(obj);
     if (obj.constructor === RegExp) return new RegExp(obj);
-    var newObj = new obj.constructor(); //保持继承链
-    for (var key in obj) {
-        if (obj.hasOwnProperty(key)) { //不遍历其原型链上的属性
-            var val = obj[key];
-            newObj[key] = typeof val === 'object' ? arguments.callee(val) : val; // 使用arguments.callee解除与函数名的耦合
-        }
+    const newObj = new obj.constructor(); // 保持继承链
+    for (const key of Object.keys(obj)) {
+      if (obj.hasOwnProperty(key)) { // 不遍历其原型链上的属性
+        const val = obj[key];
+        newObj[key] = typeof val === 'object' ? deepCloneJson(val) : val; // 使用arguments.callee解除与函数名的耦合
+      }
     }
     return newObj;
-};
+  }
 
 
 /**
